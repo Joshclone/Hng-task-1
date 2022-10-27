4dddd03eeecaaf4a2be4f23479ddddecc9c1174a
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-10-27 19:35:21 +0100

    first

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+app.use(express.json());
+
+app.get("/api",(req,res)=>{
+    res.status(200).json({
+        status: 'success',
+        data: {
+            slackUsername: "Menez",
+            backend: true,
+            age: 25,
+            bio: "Hi my name is Henry Ugochukwu Agu, i am a backend developer with one year experience and desire to network, learn more and become a pro developer"
+        }
+    })
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening at port ${PORT}`);
+})
