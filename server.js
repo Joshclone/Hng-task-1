c8dfa9cc67c20bef1885fbbc00ee377879a23e1c
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2022-10-28 11:05:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ app.use(express.json());
 app.use(cors())
 app.get("/",(req,res)=>{
     res.status(200).json({
-        status: 'success',
-        data: {
+        
+
+        
+        
+
             slackUsername: "Menez",
             backend: true,
             age: 25,
             bio: "Hi my name is Henry Ugochukwu Agu, i am a backend developer with one year experience and desire to network, learn more and become a pro developer"
-        }
+        
     })
 })
 
